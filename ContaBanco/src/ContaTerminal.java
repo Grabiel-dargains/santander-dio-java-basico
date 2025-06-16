91302609ba5680b3ed28d108f810fee79b8b8e1f
gdarg <gabrieldargains@gmail.com>
2025-06-16 12:16:11 -0300

    esqueci de fechar o scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -21,11 +21,14 @@ public class ContaTerminal {
 
         System.out.print("Qual o valor de seu depósito inicial? Insira no formato xxxx,yy \n");
         double saldo = scanner.nextDouble();
+        scanner.close();
 
         System.out.println(String.format(
             "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo %.2f já está disponível para saque.",
              nome, agencia, conta, saldo
              ));
+        
+        
 
     }
 }
